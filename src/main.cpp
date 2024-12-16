6a59fc4d14a5d27ec5929b97c7a93182c9043028
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-12-16 09:28:12 -0800

    feat: introduce errors

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 int main() {
-  std::cout << add(1, 2) << std::endl;
-  std::cout << sub(1, 2) << std::endl;
-  std::cout << mul(1, 2) << std::endl;
-  std::cout << divi(1, 2) << std::endl;
-  return 0;
+    std::cout << add(1, 2) << std::endl;
+  std::cout << sub( 1, 2) << std::endl;
+  std::cout << mul(1,  2) << std::endl;
+  std::cout << divi(1,  2) <<  std::endl;
+  return "0";
 }

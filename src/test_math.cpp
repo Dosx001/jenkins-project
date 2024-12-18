2ed5f532aa96deb76c5af986a74c3f4323ee6c5e
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-12-18 07:03:21 -0800

    fix: test

--- src/test_math.cpp
+++ src/test_math.cpp
@@ -3,25 +3,25 @@
 #include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(test_add) {
-  BOOST_TEST(add(1, 2) == 4);
+  BOOST_TEST(add(1, 2) == 3);
   BOOST_TEST(add(-1, -2) == -3);
   BOOST_TEST(add(0, 0) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(test_sub) {
-  BOOST_TEST(sub(1, 2) == 0);
+  BOOST_TEST(sub(1, 2) == -1);
   BOOST_TEST(sub(-1, -2) == 1);
   BOOST_TEST(sub(0, 0) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(test_mul) {
-  BOOST_TEST(mul(1, 2) == 3);
+  BOOST_TEST(mul(1, 2) == 2);
   BOOST_TEST(mul(-1, 2) == -2);
   BOOST_TEST(mul(0, 2) == 0);
 }
 
 BOOST_AUTO_TEST_CASE(test_divi) {
-  BOOST_TEST(divi(4, 2) == 3);
+  BOOST_TEST(divi(4, 2) == 2);
   BOOST_TEST(divi(1, 2) == 0);
   BOOST_CHECK_THROW(divi(1, 0), std::runtime_error);
 }

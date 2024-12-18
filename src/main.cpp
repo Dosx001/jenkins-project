9f92cdd3d0cf19865deb4bd7b23ff3dd900319fd
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-12-18 05:53:05 -0800

    fix: func call

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,6 @@ int main() {
   std::cout << add(1, 2) << std::endl;
   std::cout << sub(1, 2) << std::endl;
   std::cout << mul(1, 2) << std::endl;
-  std::cout << divi(1, "2") << std::endl;
+  std::cout << divi(1, 2) << std::endl;
   return 0;
 }
